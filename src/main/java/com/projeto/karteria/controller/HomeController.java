4c3a67489705312dcf12e73290287e04ba9e4795
Th3AI99la <talles.th54@gmail.com>
2025-10-01 21:29:22 -0300

    version 1.0.9

--- src/main/java/com/projeto/karteria/controller/HomeController.java
+++ src/main/java/com/projeto/karteria/controller/HomeController.java
@@ -1,18 +1,32 @@
 package com.projeto.karteria.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired; // Importe o serviço
+import org.springframework.stereotype.Controller; // Importe o Autowired
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping; // Importe o Model
+
+import com.projeto.karteria.model.Usuario;
+import com.projeto.karteria.service.UsuarioService; // Importe a List
 
 @Controller
 public class HomeController {
 
+    @Autowired // Injeta a dependência do nosso serviço de usuário
+    private UsuarioService usuarioService;
+
     @GetMapping("/")
     public String showIndexPage() {
-        return "index"; // Mostra a página pública inicial
+        return "index";
     }
 
     @GetMapping("/home")
-    public String showHomePage() {
-        return "home"; // Mostra a página principal após o login
+    public String showHomePage(Model model) {
+        List<Usuario> listaDeUsuarios = usuarioService.listarTodos();
+
+        model.addAttribute("usuarios", listaDeUsuarios);
+
+        return "home";
     }
 }
